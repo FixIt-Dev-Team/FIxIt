ae1f23bb569706a8b55da209aa3217e363331184
destiny3912 <destiny3910@gmail.com>
2022-05-09 00:51:53 +0900

    ADD : Logout system

--- app/src/main/java/com/example/fixit/welcomeActivity.java
+++ app/src/main/java/com/example/fixit/welcomeActivity.java
@@ -21,7 +21,7 @@ public class welcomeActivity extends AppCompatActivity {
 
         Button test = findViewById(R.id.welcome_test);
         Button enter = findViewById(R.id.welcome_enter);
-        Button logoutBtn;
+        Button logoutBtn = findViewById((R.id.logoutButton));
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -39,6 +39,14 @@ public class welcomeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent_enter = new Intent(getApplicationContext(), MainActivity.class);//intent 수정 필요, class 만들어서 넣기
                 startActivity(intent_enter);
+            }
+        });
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+                finishAffinity();
             }
         });
     }
